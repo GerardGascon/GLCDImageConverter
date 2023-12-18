538b0c5c1ed08487991c87d452cff8b501ce94dc
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2023-12-18 10:49:42 +0100

    Init

---
+++ GLCDImageConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace GLCDImageConverter;
+
+class Program {
+	static void ReadPixels(string file) {
+		Bitmap img = new(file);
+		for (int i = 0; i < img.Width; i++) {
+			for (int j = 0; j < img.Height; j++) {
+				Color pixel = img.GetPixel(i, j);
+
+				Console.Write(pixel.A == 0 ? "0" : "1");
+			}
+		}
+	}
+	
+	static void Main(string[] args) {
+		if(args.Length == 0) {
+			Console.WriteLine("No image file specified.");
+			return;
+		}
+
+		ReadPixels(args[0]);
+	}
+}
