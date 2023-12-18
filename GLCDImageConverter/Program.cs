3519c8c0d05bda1e2ed14bd4af91eec073460f89
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2023-12-18 11:24:03 +0100

    Added conversion code

--- GLCDImageConverter/Program.cs
+++ GLCDImageConverter/Program.cs
@@ -3,15 +3,39 @@
 namespace GLCDImageConverter;
 
 class Program {
-	static void ReadPixels(string file) {
+	static byte[]? ReadPixels(string file) {
 		Bitmap img = new(file);
-		for (int i = 0; i < img.Width; i++) {
-			for (int j = 0; j < img.Height; j++) {
-				Color pixel = img.GetPixel(i, j);
 
-				Console.Write(pixel.A == 0 ? "0" : "1");
+		if (img.Width != 128 || img.Height != 64) {
+			Console.WriteLine("Image must be 128x64 pixels.");
+			return null;
+		}
+        
+		byte[]? pixels = new byte[1024];
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < img.Width; j++) {
+				byte px = 0;
+				for (int k = i * 8, l = 0; k < (i + 1) * 8; k++, l++) {
+					Color pixel = img.GetPixel(j, k);
+					px |= Convert.ToByte((pixel.A == 0 ? 0 : 1) << l);
+				}
+				pixels[i * img.Width + j] = px;
 			}
 		}
+
+		return pixels;
+	}
+
+	static void ExportImage(byte[]? pixels) {
+		if (pixels == null) return;
+		using StreamWriter file = File.CreateText("fileName.txt");
+		file.Write("unsigned const char bitmap [1024] = { ");
+		foreach (byte t in pixels) {
+			file.Write($"{t}, ");
+		}
+		file.WriteLine("};");
+		file.Close();
 	}
 	
 	static void Main(string[] args) {
@@ -20,6 +44,7 @@ class Program {
 			return;
 		}
 
-		ReadPixels(args[0]);
+		byte[]? pixels = ReadPixels(args[0]);
+		ExportImage(pixels);
 	}
 }
