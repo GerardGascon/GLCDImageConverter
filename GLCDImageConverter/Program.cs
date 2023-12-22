a239b014b0477b7fc31892cf224def0fbe810897
Geri <ggasconmoline@gmail.com>
2023-12-22 16:17:49 +0100

    refactor: Extract ImageGeneration class

--- GLCDImageConverter/Program.cs
+++ GLCDImageConverter/Program.cs
@@ -1,66 +1,8 @@
-﻿using System.Drawing;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace GLCDImageConverter;
 
 partial class Program {
-	static byte[]? ReadPixels(string file, out int imageWidth) {
-		Bitmap img = new(file);
-
-		if (img.Width > 128 || img.Height > 64) {
-			Console.WriteLine("ERROR: Image must be less or equal than 128x64 pixels.");
-			imageWidth = -1;
-			return null;
-		}
-
-		if (img.Height % 8 != 0) {
-			Console.WriteLine("ERROR: The image height must be a multiple of 8.");
-			imageWidth = -1;
-			return null;
-		}
-        
-		byte[] pixels = new byte[img.Width * img.Height / 8];
-
-		for (int i = 0; i < img.Height / 8; i++) {
-			for (int j = 0; j < img.Width; j++) {
-				byte px = 0;
-				for (int k = i * 8, l = 0; k < (i + 1) * 8; k++, l++) {
-					Color pixel = img.GetPixel(j, k);
-					px |= Convert.ToByte((pixel.A == 0 ? 0 : 1) << l);
-				}
-				pixels[i * img.Width + j] = px;
-			}
-		}
-
-		imageWidth = img.Width;
-		return pixels;
-	}
-
-	static void ExportImage(IReadOnlyCollection<byte>? pixels, string fileName, int imageWidth) {
-		if (pixels == null) {
-			Console.WriteLine("An error occurred while exporting the array.");
-			return;
-		}
-		
-		using StreamWriter file = File.CreateText($"{fileName}.txt");
-		file.WriteLine("#include \"GLCD.h\"\n");
-		file.Write($"unsigned const char {fileName}[{pixels.Count}] = {{ ");
-		foreach (byte t in pixels) {
-			file.Write($"{t}, ");
-		}
-		file.WriteLine("};\n");
-		
-		file.WriteLine($"void draw_{fileName}(){{");
-		file.WriteLine($"\tfor (int i = 0; i < {pixels.Count}; ++i) {{");
-		file.WriteLine($"\t\twriteByte(i / {imageWidth}, i % {imageWidth}, {fileName}[i]);");
-		file.WriteLine("\t}");
-		file.WriteLine("}");
-		
-		file.Close();
-
-		Console.WriteLine("Image exported successfully.");
-	}
-	
 	static void Main(string[] args) {
 		if(args.Length == 0) {
 			Console.WriteLine("ERROR: No image file specified.");
@@ -76,8 +18,8 @@ partial class Program {
 			return;
 		}
 
-		byte[]? pixels = ReadPixels(args[0], out int imageWidth);
-		ExportImage(pixels, fileName, imageWidth);
+		byte[]? pixels = ImageGeneration.ReadPixels(args[0], out int imageWidth);
+		ImageGeneration.ExportImage(pixels, fileName, imageWidth);
 		
 		Console.WriteLine("Program execution completed, press any key to exit.");
 		Console.ReadKey();
